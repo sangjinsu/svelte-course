49d8c009cbbec056cd2c902f24de3eb05b3973f2
sangjinsu <climbplant39@kakao.com>
2023-01-08 18:49:17 +0900

    ✨ button component example

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,16 @@
 <script>
-    import Counter from "./lib/Counter.svelte";
+    import Button from "./lib/Button.svelte";
+    import IoIosArrowDropdown from 'svelte-icons/io/IoIosArrowDropdown.svelte'
+
 </script>
 
-<Counter initCount={5} maxCount={10}/>
-
-
+<Button on:click|once={(event) => {
+    alert(true)
+}
+} let:x >
+    <div style:width="20px">
+        <IoIosArrowDropdown slot="left"></IoIosArrowDropdown>
+    </div>
+    Button Test {x}</Button>
 <style>
-    h1 {
-        color: red
-    }
 </style>
---
+++ src/lib/Button.svelte
@@ -0,0 +1,12 @@
+<script>
+    let x
+</script>
+
+<button on:click class="flex bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" {...$$restProps}>
+    {#if $$slots.left}
+        <div>
+            <slot name="left"></slot>
+        </div>
+    {/if}
+    <slot x={x}>Fallback</slot>
+</button>
