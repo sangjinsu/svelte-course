6c82e1855db4925e4193316bbbe3e240fc572002
sangjinsu <climbplant39@kakao.com>
2023-01-08 18:00:57 +0900

    :tada: init course

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Counter from "./lib/Counter.svelte";
+</script>
+
+<Counter initCount={5} maxCount={10}/>
+
+
+<style>
+    h1 {
+        color: red
+    }
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let initCount: number = 0
+    export let maxCount;
+
+    let count = initCount
+    const increment = () => {
+        if (count === maxCount) return
+        count += 1
+    }
+
+
+    $:string = `hi ${count}`
+    let string;
+
+</script>
+
+<button on:click={increment}>
+    count is {count}
+</button>
+<p>{string}</p>
