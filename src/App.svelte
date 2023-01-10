65f7abba7a3c665fb506dcb59f6f9dd9623d751b
sangjinsu <climbplant39@kakao.com>
2023-01-10 23:02:39 +0900

    ✨ 1월 10일

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
-<script>
+<script lang="ts">
     import TodoList from "./lib/TodoList.svelte";
     import {v4 as uuid} from "uuid";
-
 
     let todos = [
         {
@@ -23,12 +22,35 @@
         }
     ]
 
-    $: console.log(todos)
+    const handleAddTodo = (event) => {
+        event.preventDefault()
+        todos = [...todos, {
+            id: uuid(),
+            title: event.detail.title,
+            completed: false
+        },]
+    }
+
+    const handleRemoveTodo = (event) => {
+        todos = todos.filter(todo => todo.id !== event.detail.id)
+    }
+
+    const handleToggleTodo = (event) => {
+        todos = todos.map(todo => {
+            if (todo.id == event.detail.id){
+                return {...todo, completed: event.detail.value}
+            }
+            return {...todo}
+        })
+    }
+
 </script>
 
 <h2>{todos.length}</h2>
-<TodoList bind:todos></TodoList>
-<button on:click={() => {todos = [];}}>Update Todos</button>
+<TodoList {todos} on:addtodo={handleAddTodo}
+          on:removetodo={handleRemoveTodo}
+          on:toggletodo={handleToggleTodo}
+></TodoList>
 
 <style>
 </style>
--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -1,34 +1,45 @@
-<script>
-    import {v4 as uuid} from "uuid";
+<svelte:options immutable={true}/>
+
+<script lang="ts">
+    import {createEventDispatcher} from "svelte";
+
 
     export let todos = []
 
     let inputText = ''
+
+    const dispatch = createEventDispatcher()
+
     const handleSubmit = () => {
-        if (!inputText) {
-            return
-        }
-        // todos.push(
-        //     {
-        //         id: uuid(),
-        //         completed: false ,
-        //         title: inputText
-        //     }
-        // )
-        // todos = todos
-        todos = [...todos, {
-            id: uuid(),
-            completed: false,
+        dispatch('addtodo', {
             title: inputText
-        }]
+        }, {cancelable: true})
         inputText = ''
+    }
+
+    const handleRemoveTodo = (id: string) => {
+        dispatch('removetodo', {id})
+    }
+
+    const handleToggleTodo = (id, value) => {
+        dispatch('toggletodo', {id, value})
     }
 </script>
 
 <div>
     <ul>
-        {#each todos as {id, title}, index (index)}
-            <li>{index + 1} - {title}</li>
+        {#each todos as {id, title, completed} (id)}
+            <li>
+                <label>
+                    <input type="checkbox"
+                           on:input={event => {event.currentTarget.checked = completed
+                            handleToggleTodo(id, !completed)
+                           }}
+                           checked={completed}>
+                    {title}
+                </label>
+                <button on:click={()=>handleRemoveTodo(id)}>Remove</button>
+            </li>
         {/each}
     </ul>
     <form on:submit|preventDefault={handleSubmit}>
